c4faea2e1f2bf49416a5d86edd9f971bc03844b3
Rafael de Elvira <relviratellez@gmail.com>
2016-01-14 23:21:41 +0100

    almost finished upload_pending_to_plex.sh

--- upload_pending_to_plex.sh
+++ upload_pending_to_plex.sh
@@ -13,24 +13,29 @@ echo "- PLEX SERVER FILE UPLOADER (PENDING DOWNLOADS) -"
 echo "-------------------------------------------------"
 echo " "
 
-# TODO: Look if there are any files in the torrent download folder
-
-# Check if Plex server is available
-if ping -c 1 $PLEX_SERVER &> /dev/null
+if [ "$(ls -A $INSPECT_DIR)" ]
 then
-	echo "> Plex server is UP and running!!"
-	echo "> Uploading pending files to Plex server"
-	scp -r $INSPECT_DIR/* ret@$PLEX_SERVER:$PLEX_SERVER_DESTINATION_DIR
-	if [ "$?" -eq "0" ];
+	FILECOUNT=$(ls -l $INSPECT_DIR | wc -l)
+  	echo "Inspected folder contains: $FILECOUNT files"
+  	
+	if ping -c 1 $PLEX_SERVER &> /dev/null
 	then
-	    echo "> Upload OK! Deleting pending files..."
-	    #NOT YET rm -rf $INSPECT_DIR/*
+		echo "Plex server is UP and running!!"
+		echo "Uploading pending files to Plex server"
+		scp -r $INSPECT_DIR/* ret@$PLEX_SERVER:$PLEX_SERVER_DESTINATION_DIR
+		if [ "$?" -eq "0" ];
+		then
+		    echo "Upload OK! Deleting pending files..."
+		    #rm -rf $INSPECT_DIR/*
+		else
+		    echo "Upload FAILED, we're keeping the files for the moment"
+		fi
+		
 	else
-	    echo "> Upload FAILED, we're keeping the files for the moment"
+	  echo "Plex server is DOWN, we'll try later..."
 	fi
-	
 else
-  echo "> Plex server is DOWN, we'll try later..."
+  	echo "Inspected folder is empty, ignoring.."
 fi
 
 
